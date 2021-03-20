c492c1dd6e77fa35922d53d872ea44599eb0bdc4
Saadkhalid913 <43319921+Saadkhalid913@users.noreply.github.com>
2021-03-19 21:34:56 -0400

    Update 3D Surface Area.py

--- Github Data Structures and Algos/HACKERANK/3D Surface Area.py
+++ Github Data Structures and Algos/HACKERANK/3D Surface Area.py
@@ -1,10 +1,8 @@
-# https://www.hackerrank.com/challenges/3d-surface-area/problem
-def main():
-  m,n = tuple(map(int, input().split()))
-  DP =[]
-  for _ in range(m):
-      DP.append(list(map(int, input().split())))
-  SA = 0 
+m,n = tuple(map(int, input().split()))
+DP =[]
+for _ in range(m):
+    DP.append(list(map(int, input().split())))
+SA = 0 
 
 def validateSides(i,j,DP, SA):
     top = False 
@@ -23,7 +21,7 @@ def validateSides(i,j,DP, SA):
     if j + 1 >= n:
         SA += DP[i][j]
         right = True 
-
+        
     if not right:
         SA += max(0,DP[i][j] - DP[i][j+1])
     if not left:
@@ -32,21 +30,18 @@ def validateSides(i,j,DP, SA):
         SA += max(0,DP[i][j] - DP[i-1][j])
     if not bottom:
         SA += max(0,DP[i][j] - DP[i+1][j])
-
+    
     return SA 
+        
+        
 
 
-
-
-  for i in range(m):
-      for j in range(n):
-          SA = validateSides(i,j,DP,SA)
-
-  print(SA + 2*(m*n))   
- 
-if __name__=="__main__":
-  main()
+for i in range(m):
+    for j in range(n):
+        SA = validateSides(i,j,DP,SA)
+        
+print(SA + 2*(m*n))          
 
         
         
-       
+        
