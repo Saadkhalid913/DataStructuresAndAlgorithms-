f94720abf1738ace8d13bb067a9391982fd38a7f
Saadkhalid913 <43319921+Saadkhalid913@users.noreply.github.com>
2021-03-24 15:55:52 -0400

    Add files via upload

---
+++ Github Data Structures and Algos/Data Structures/Stacks/Stacks.py
@@ -0,0 +1,22 @@
+class Stack(list):
+    def __init__(self):
+        super().__init__()
+
+    def push(self, i):
+        self.append(i)
+
+    def isempty(self):
+        return len(self) == 0
+
+    def peek(self):
+        return self[-1]
+
+
+if __name__ == "__main__":
+    q = Stack()
+    q.push(1)
+    q.push(2)
+    q.push(3)
+    print(q)
+    print(q.pop())
+    print(q)
